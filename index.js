9806653dc246222f3c6ed26af432d4e927f048d8
Marcus Urani <marcus.urani20@gmail.com>
2022-03-08 15:01:03 -0300

    Refazendo o projeto, com novas funcionalidades

--- index.js
+++ index.js
@@ -1,39 +1,72 @@
-//Limpando o console.
-console.clear();
+const periodsInput = document.querySelector("#period__input");
+const formSubmitButton = document.querySelector("#submit__button");
+const form = document.querySelector(".main__form");
+const result = document.querySelector(".main__result");
+const resultContainer = document.querySelector(".main__result__container");
+const nameInput = document.querySelector("#name__input");
+const averageInput = document.querySelector("#average__input");
 
-var nome = document.querySelector("#nome");
-console.log(nome.value);
+formSubmitButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  form.innerHTML = "";
+  const periods = periodsInput.value;
+  const averageValue = averageInput.value;
+  if (periods && periods < 5 && periods > 1) {
+    formSubmitButton.disabled = true;
+    loopInputs(periods);
+    form.innerHTML += `<button class="main__button" id="calc__button" type="submit" onclick="calcAverage(${periods}, event, ${averageValue})">Calcular</button>`;
+  } else {
+    form.innerHTML = `<p class="main__error">Preencha os campos corretamente</p>`;
+  }
+});
 
-//Função responsável pelo cálculo da média.
-function calculaMedia() {
-    var notaPrimeiroBimestre = parseFloat(document.querySelector(".notaPrimeiro").value);
-    var notaSegundoBimestre = parseFloat(document.querySelector(".notaSegundo").value);
-    var notaTerceiroBimestre = parseFloat(document.querySelector(".notaTerceiro").value);
-    var notaQuartoBimestre = parseFloat(document.querySelector(".notaQuarto").value);
+const calcAverage = (periods, event, averageValue) => {
+  event.preventDefault();
+  const nameValue = nameInput.value;
+  let gradeValues = [];
+  for (let i = periods; i > 0; i--) {
+    const input = document.querySelector(`#period__${i}`);
+    gradeValues.push(input.value);
+  }
+  const average =
+    gradeValues.reduce((acc, cur) => Number(acc) + Number(cur), 0) / periods;
+  insertResult(average, averageValue, nameValue);
+};
 
-    //Variável utilizada para a soma das notas.
-    var somaNota =
-        notaPrimeiroBimestre +
-        notaSegundoBimestre +
-        notaTerceiroBimestre +
-        notaQuartoBimestre;
+const insertResult = (average, averageValue, nameValue) => {
+  if (average >= averageValue) {
+    result.classList.remove("disabled");
+    result.classList.add("green");
+    resultContainer.innerHTML = `<p class="main__result-text">Parabéns ${nameValue}, você foi aprovado!</p>
+  <p class="main__result-text">Sua média final é ${average.toFixed(1)}</p>
+  <button class="main__button close__button" onclick="closeModal()">Fechar</button>
+  `;
+  } else {
+    result.classList.remove("disabled");
+    result.classList.add("red");
+    resultContainer.innerHTML = `<p class="main__result-text">Infelizmente você foi reprovado, ${nameValue}!</p>
+  <p class="main__result-text">Sua média final é ${average.toFixed(1)}</p>
+  <button class="main__button close__button" onclick="closeModal()">Fechar</button>`;
+  }
+};
 
-    //Variável para calcular a média das notas.
-    var calculaMedia = somaNota / 4;
+const loopInputs = (periods) => {
+  for (let i = periods; i > 0; i--) {
+    const newLabel = document.createElement("label");
+    const newInput = document.createElement("input");
+    newLabel.textContent = `${i}º Período`;
+    newLabel.htmlFor = `period__${i}`;
+    newLabel.classList.add("main__label");
+    newInput.id = `period__${i}`;
+    newInput.classList.add("main__input");
+    newInput.placeholder = "Digite a nota do período";
+    newInput.required = true;
+    newInput.type = "number";
+    form.appendChild(newLabel);
+    form.appendChild(newInput);
+  }
+};
 
-    //Variável para corrigir valores decimais.
-    var notaFinal = calculaMedia.toFixed(1);
-    // var notaFinalCorrigida = parseInt(notaFinal);
-
-    console.log(notaFinal);
-
-    //Função responsável por conferir se o aluno está aprovado ou não.
-    if (notaFinal >= 5) {
-        var mediaFinal = `Parabéns, você foi aprovado, ${nome.value}, sua média final   foi ${notaFinal}`;
-        document.querySelector("#mediaFinal").innerHTML = mediaFinal;
-    } else {
-        var mediaFinal = `Infelizmente você não foi aprovado ${nome.value}, sua média   final foi ${notaFinal}`;
-        document.querySelector("#mediaFinal").innerHTML = mediaFinal;
-    }
-}
-
+const closeModal = () => {
+  result.classList.add("disabled");
+};
